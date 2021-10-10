6e1df6d5043cdd5c93679445e4b5967bb837bd11
robtop100 <37734780+robtop100@users.noreply.github.com>
2021-10-10 11:00:42 +0200

    Add project files.

---
+++ WebMvc1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMvc1.Models;
+
+namespace WebMvc1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]  //reaguj tylko na HTTP POST konkretnie: POST /Home/Form1
+        public IActionResult Form1(FormModel model) //spodziewmy sie pol Name,Description, IsVisible
+        {
+            return View(model); //odebrane dane (zapisane w model) przekaz do widoku Views/Home/Form1.cshtml
+        }
+
+    }
+}
